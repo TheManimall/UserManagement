60c543238a65a0a93f9ae9b87b037433b554250e
TheManimall <yuriyvovk5@gmail.com>
2018-11-04 23:31:06 +0200

    remove redux-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import routes from '../routes/index';
+import UsersList from '../containers/UserList';
+import NavBar from './NavBar';
+import AddUser from '../containers/AddUserCont';
+import UserInfoCont from '../containers/UserInfoCont';
+import GroupList from '../containers/GroupList';
+import GroupInfoCont from '../containers/GroupInfoCont';
+import AddGroup from '../containers/AddGroupCont';
 
-const App = ({ history }) => {
+const App = () => {
   return (
-    <ConnectedRouter history={history}>
-      {routes}
-    </ConnectedRouter>
+    <div>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={NavBar} />
+      <Route path="/users" component={UsersList} />
+      <Route path="/add-user" component={AddUser} />
+      <Route path="/user/:id" component={UserInfoCont} />
+      <Route path="/groups" component={GroupList} />
+      <Route path="/group/:id" component={GroupInfoCont} />
+      <Route path="/add-group" component={AddGroup} />
+    </Switch>
+    </div>
   );
 };
 
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 
 import UsersList from '../containers/UserList';
 import NavBar from '../components/NavBar';
