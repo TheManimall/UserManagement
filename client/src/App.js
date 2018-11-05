3de118358628af4e5bbcd05dbc4a449a3acec043
TheManimall <yuriyvovk5@gmail.com>
2018-11-05 15:07:35 +0200

    add minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import UserContainer from '../User/UserContainer';
-import NavBar from '../NavBar/NavBar';
-import AddUserContainer from '../AddUser/AddUserContainer';
-import UserInfoContainer from '../UserInfo/UserInfoContainer';
-import GroupContainer from '../Group/GroupContainer';
-import GroupInfoContainer from '../GroupInfo/GroupInfoContainer';
-import AddGroupContainer from '../AddGroup/AddGroupContainer';
+import UserContainer from './User/UserContainer';
+import NavBar from './NavBar/NavBar';
+import AddUserContainer from './AddUser/AddUserContainer';
+import UserInfoContainer from './UserInfo/UserInfoContainer';
+import GroupContainer from './Group/GroupContainer';
+import GroupInfoContainer from './GroupInfo/GroupInfoContainer';
+import AddGroupContainer from './AddGroup/AddGroupContainer';
 
 
 const App = () => {
