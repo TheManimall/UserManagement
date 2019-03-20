423d68149710848bd46389ed9e2b56b72071623b
Lobster <yuriyvovk5@gmail.com>
2019-03-20 16:14:28 +0200

    add tests to Group Info

---
+++ client/src/GroupInfo/duck/test.js
@@ -0,0 +1,48 @@
+import actions from './actions';
+import types from './types';
+
+describe('actions', () => {
+  it('should create an action to get group info', () => {
+
+    const user = [
+      { groupId: ['group id test1', 'group id test1'], _id: 'testId1', firstName: 'TestName1', lastName: 'TestLastName1' },
+      { groupId: ['group id test2', 'group id test1'], _id: 'testId2', firstName: 'TestName2', lastName: 'TestLastName2' },
+      { groupId: ['group id test3', 'group id test2'], _id: 'testid3', firstName: 'TestName3', lastName: 'TestLastName3' },
+    ];
+
+    const group = {
+      _id: 'Test group id 1',
+      groupName: 'Test group name 1',
+    };
+
+    const expectedAction = {
+      type: types.GET_GROUP_INFO,
+      payload: {
+        user,
+        group,
+      },
+    };
+
+    expect(actions.getGroupInfoSuccess(group, user)).toEqual(expectedAction);
+  });
+});
+
+describe('actions', () => {
+  it('should create an action to add user to group', () => {
+
+    const user = { 
+      groupId: ['group id test1', 'group id test1'], 
+      _id: 'testId1',
+      firstName: 'TestName1',
+      lastName: 'TestLastName1',
+    };
+
+
+    const expectedAction = {
+      type: types.ADD_USER_TO_GROUP,
+      payload: user,
+    };
+
+    expect(actions.addUserToGroupSuccess(user)).toEqual(expectedAction);
+  });
+});
